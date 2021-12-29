06f911083f2eafae035be4709fc47c68b99334d6
omervered0708 <omer.vered@campus.technion.ac.il>
2021-12-29 19:39:37 +0200

    added connection

--- Pokemons_App/views.py
+++ Pokemons_App/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.db import connection
 # Create your views here.
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
