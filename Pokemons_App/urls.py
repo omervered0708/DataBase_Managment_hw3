7fc67d5b08842b8bc66bfaed0c484df6f073c42a
omervered0708 <omer.vered@campus.technion.ac.il>
2022-01-03 20:29:27 +0200

    added connection

--- Pokemons_App/urls.py
+++ Pokemons_App/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('query_results', views.query_results, name='query_results'),
-    path('pokemon_add', views.pokemon_add, name='pokemon_add')
+    path('pokemon_add', views.pokemon_add, name='pokemon_add'),
+    path('index', views.index, name="index")
 ]
