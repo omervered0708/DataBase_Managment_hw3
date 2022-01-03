5fe32f52503e28add0a075ee4b7b724241f94228
omervered0708 <omer.vered@campus.technion.ac.il>
2022-01-03 16:29:34 +0200

    added connection

--- Pokemons_App/views.py
+++ Pokemons_App/views.py
@@ -5,3 +5,7 @@ def dictfetchall(cursor):
     "Return all rows from a cursor as a dict"
     columns = [col[0] for col in cursor.description]
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+def index(request):
+    return render(request, "index.html")
+
